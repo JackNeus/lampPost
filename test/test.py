d35e03c55b1c26626ca91ea882f154c0dbd3b131
Jack Neus <jack.neus@gmail.com>
2018-04-04 21:10:27 -0400

    Added tests for invalid events, and fixed bugs that I found as a result of said testing.

--- test/test.py
+++ test/test.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import dateutil.parser
 import functools
 import json
@@ -58,17 +59,20 @@ def compare_events(source, record):
 				return False
 	return True
 
+def make_add_event_request(event_data):
+	r = requests.put(app_url + "/event/add", json=event_data)
+	assert r.status_code == 200
+	return get_data(r)
+
 valid_events = {}
 
 def test_add_valid_events():
 	with open('test/test_data.json', 'r') as f:
 		data = json.load(f)
-	for entry in data:
-		r = requests.put(app_url + "/event/add", json=entry)
-		assert r.status_code == 200
-		r = get_data(r)
+	for event in data:
+		r = make_add_event_request(event)
 		assert r["status"] == "Success"
-		valid_events[r["data"]["id"]] = entry
+		valid_events[r["data"]["id"]] = event
 
 def test_get_valid_events():
 	for event_id in valid_events:
@@ -84,9 +88,73 @@ def test_delete_valid_events():
 		assert r.status_code == 200
 		r = get_data(r)
 
+def test_add_invalid_events():
+	base_event = {"title": "Party", "host":"LampPost Team", "creator": "bwk",
+				  "description": "This event should cause some trouble.",
+				  "instances": [{"start_datetime": "3pm April 1 2100",
+				  				 "end_datetime": "4pm April 1 2100",
+				  				 "location": "Princeton University"},
+				  				 {"start_datetime": "3pm April 2 2100",
+				  				 "end_datetime": "4pm April 2 2100",
+				  				 "location": "Princeton University"}]}
+	events = []
+	
+	# Missing required fields.
+	for field in ["title", "host", "creator", "description", "instances"]:
+		# Remove field.
+		no_value = deepcopy(base_event)
+		del no_value[field]
+		r = make_add_event_request(no_value)
+		assert r["status"] == "Error"
+	# String fields type check.
+	for field in ["title", "host", "creator", "description"]:
+		# Incorrectly-typed value.
+		bad_value = deepcopy(base_event)
+		bad_value[field] = 123
+		r = make_add_event_request(bad_value)
+		assert r["status"] == "Error"
+	# String fields length check.
+	for field, length in [("title", 5), ("host",3), ("description", 10)]:
+		# Insufficiently long value.
+		short_value = deepcopy(base_event)
+		short_value[field] = "A"*(length-1)
+		r = make_add_event_request(short_value)
+		assert r["status"] == "Error"
+	
+	# Instances tests.
+
+	# End datetime earlier than start datetime.
+	time_swap = deepcopy(base_event)
+	time_swap["instances"][0]["start_datetime"] = base_event["instances"][0]["end_datetime"]
+	time_swap["instances"][0]["end_datetime"] = base_event["instances"][0]["start_datetime"]
+	r = make_add_event_request(time_swap)
+	assert r["status"] == "Error"
+	
+	# Missing required fields.
+	for field in ["location", "start_datetime", "end_datetime"]:
+		no_value = deepcopy(base_event)
+		del no_value["instances"][0][field]
+		r = make_add_event_request(no_value)
+		assert r["status"] == "Error"
+
+	# Insufficiently long value.
+	short_value = deepcopy(base_event)
+	short_value["instances"][0]["location"] = "AB"
+	r = make_add_event_request(short_value)
+	assert r["status"] == "Error"
+
+	# Extraneous fields.
+	extra_field = deepcopy(base_event)
+	extra_field["bad_field_does_not_exist"] = "uh oh"
+	r = make_add_event_request(extra_field)
+	assert r["status"] == "Error"
+
+# TODO: add search tests
+
 tests = [test_add_valid_events,
 test_get_valid_events,
-test_delete_valid_events]
+test_delete_valid_events,
+test_add_invalid_events]
 
 if __name__ == '__main__':
 	failed = 0
